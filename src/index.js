f4137b852985f6601616b8c2e4b74197e19d300c
MirThomasson <mirthomasson@gmail.com>
2022-08-15 10:54:17 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';\
 import App from './App';
 import {createRoot} from 'react-dom/client';
 import {StrictMode} from 'react';
